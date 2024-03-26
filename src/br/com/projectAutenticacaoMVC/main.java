fb4b3b1e5d2737aaf537816979a6f9f83e42ceb1
Luiz MAdruga <madruga_emo@hotmail.com>
2024-03-25 22:07:04 -0300

    correcao

--- src/br/com/projectAutenticacaoMVC/main.java
+++ src/br/com/projectAutenticacaoMVC/main.java
@@ -1,5 +1,19 @@
 package br.com.projectAutenticacaoMVC;
 
+import br.com.autenticacao.controller.UsuarioController;
+import br.com.autenticacaoModel.Usuario;
+
 public class main {
 
+	public static void main() {
+
+		Usuario usuario = new Usuario();
+		usuario.setNome("Gui");
+		usuario.setEmail("guilherme@proway");
+		usuario.setSenha("12345");
+
+		UsuarioController controller = new UsuarioController();
+		controller.cadastrar(usuario);
+
+	}
 }
